205b2886e890f52fec1a98afe167069b74b37a3f
ljespo <ljespo@rams.colostate.edu>
2020-12-03 12:01:09 -0700

    routes done 12/3

---
+++ src/components/CarCreate.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Create Car</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from './components/Home.vue';
+import Account from './components/Account.vue';
+import Login from './components/Login.vue';
+import CarDetail from './components/CarDetail.vue';
+import Cars from './components/Cars.vue';
+import NotFound from './components/NotFound.vue';
+import CarCreate from './components/CarCreate.vue';
+import SignUp from './components/SignUp.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/', component: Home},
+    {path: '/account', component: Account},
+    {path: '/signin', component: Login},
+    {path: '/cars', component: Cars},
+    {path: '/cars/:pk', component: CarDetail,
+        children:[
+            {path: 'car', component: CarCreate}
+        ]},
+    {path: '/signup', component: SignUp},
+    {path: '/:invalidroute(.*)', component: NotFound}
+]
+
+export default new VueRouter({mode: 'history', routes})

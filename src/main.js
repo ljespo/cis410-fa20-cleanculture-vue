c305a9f48550a43e100951a67e12c1658bdebf34
ljespo <ljespo@rams.colostate.edu>
2020-12-03 12:53:42 -0700

    axios, signup route 12/3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 import router from './routes.js'
+
+axios.defaults.baseURL = "https://cis410-fa20-esposito-api.azurewebsites.net";
 
 new Vue({
   el: '#app',
